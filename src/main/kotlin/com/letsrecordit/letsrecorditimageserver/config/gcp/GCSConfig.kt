e338c78c5f354a82450fc985d88b4a9ac25ae368
rt3310 <btac3310@gmail.com>
2025-08-26 21:26:08 +0900

    fix: application-test.yml update

--- src/main/kotlin/com/letsrecordit/letsrecorditimageserver/config/gcp/GCSConfig.kt
+++ src/main/kotlin/com/letsrecordit/letsrecorditimageserver/config/gcp/GCSConfig.kt
@@ -6,9 +6,11 @@ import com.google.cloud.storage.StorageOptions
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.util.ResourceUtils
 
 @Configuration
+@Profile("!test")
 class GCSConfig(
     @Value("\${spring.cloud.gcp.storage.credentials.location}")
     private val keyFileName: String,
