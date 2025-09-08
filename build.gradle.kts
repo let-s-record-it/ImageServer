59da86a733deb35155f438e3f10539c9874aef48
sypark <soun997@gmail.com>
2025-09-08 17:37:48 +0900

    chore[#1]: aop, actuator, prometheus 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,6 +50,13 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // AOP
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    // Actuator
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    // Prometheus
+    implementation("io.micrometer:micrometer-registry-prometheus")
 }
 
 dependencyManagement {
